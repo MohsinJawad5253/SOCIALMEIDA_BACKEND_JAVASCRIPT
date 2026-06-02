dc4c879e62b88b6e254e0d96386f5dadcdc56219
MOHSIN-JAWADWALA <mohsinjawadwala8@gmail.com>
2026-06-02 21:59:15 +0530

    Add ApiError ApiResponse and Asynchandler into utils

--- src/db/db.js
+++ src/db/db.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 async function connectDB() {
     try {
+        console.log(" \n Connecting to MongoDB...")
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(` \n ⚙️ MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
 
